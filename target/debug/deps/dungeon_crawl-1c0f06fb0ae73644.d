0012dc433bad8837f15f6221e75ed730580c0214
Orubebe Tamarah <paulorubebe@yahoo.com>
2024-02-03 05:30:49 +0100

    Create player struct and add functionality to render player to screen

--- target/debug/deps/dungeon_crawl-1c0f06fb0ae73644.d
+++ target/debug/deps/dungeon_crawl-1c0f06fb0ae73644.d
@@ -1,6 +1,7 @@
-C:\Users\User\Desktop\DEV\Personal\rust_game_dev\dungeon_crawl\target\debug\deps\dungeon_crawl-1c0f06fb0ae73644.rmeta: src\main.rs src\map.rs
+C:\Users\User\Desktop\DEV\Personal\rust_game_dev\dungeon_crawl\target\debug\deps\dungeon_crawl-1c0f06fb0ae73644.rmeta: src\main.rs src\map.rs src\player.rs
 
-C:\Users\User\Desktop\DEV\Personal\rust_game_dev\dungeon_crawl\target\debug\deps\dungeon_crawl-1c0f06fb0ae73644.d: src\main.rs src\map.rs
+C:\Users\User\Desktop\DEV\Personal\rust_game_dev\dungeon_crawl\target\debug\deps\dungeon_crawl-1c0f06fb0ae73644.d: src\main.rs src\map.rs src\player.rs
 
 src\main.rs:
 src\map.rs:
+src\player.rs:
